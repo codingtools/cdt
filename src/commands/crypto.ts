5216c26fef56395b4eeaf0d949f999a8a66a31bf
ashish <ashishpatel0720@gmail.com>
2019-10-28 03:08:07 +0530

    [CRYPTO]: added crypto functionality
    Signed-off-by: ashish <ashishpatel0720@gmail.com>

--- src/commands/crypto.ts
+++ src/commands/crypto.ts
@@ -1,91 +1,123 @@
 import {Command, flags} from '@oclif/command'
 import * as CryptoJS from 'crypto-js'
+import {JsonFormatter} from 'tslint/lib/formatters'
 
 import Utilities from '../utilities/Utilities'
 import Logger from '../utilities/Logger'
+import Hash from './hash'
 
 export default class Crypto extends Command {
+  static ENCRYPTION = 'encryption'
+  static DECRYPTION = 'decryption'
+
   static description = 'Encryption and Decryption functionality'
   static flags = {
     help: flags.help({char: 'h'}),
 
-    encryption: flags.string({char: 'e', description: 'encryption type'}),
-    decryption: flags.string({char: 'd', description: 'decryption type'}),
+    encryption: flags.string({char: 'e', description: 'encryption type, Supported [AES, DES, 3DES, Rabbit, RC4, RC4Drop]'}),
+    decryption: flags.string({char: 'd', description: 'decryption type, Supported [AES, DES, 3DES, Rabbit, RC4, RC4Drop]'}),
     string: flags.string({char: 's' , description: 'string to be encrypted/decrypted'}),
     file: flags.string({char: 'f' , description: 'file to be encrypted/decrypted'}),
     key: flags.string({char: 'k' , description: 'key for encryption/decryption'}),
+    mode: flags.string({char: 'm' , description: 'Block Mode, Supported [CBC, CFB, CTR, OFB, ECB]'})
   }
 
+  static args = [{name: 'string'}]
+
+  //need INPUT_STRING, TYPE_OF_CRYPTO , KEY, MODE
   async run() {
     const {args, flags} = this.parse(Crypto)
 
-    let enc = CryptoJS.DES.encrypt('Message', 'Secret Passphrase').ciphertext
+    args.string = Hash.getInputString(this,flags,args) //always add input to args
+    args.type = flags.encryption ? flags.encryption : flags.decryption //type like AES,DES
 
-    this.log(enc)
+    this.checkParameters(flags,args)
+    flags.encryption  ? this.Encrypt(flags, args) : this.Decrypt(flags, args)
 
-    // if -s or -f is not passed we will take it from args
-    let str = ''
-
-    const {key, decryption, string, file, encryption} = flags
-
-    if (string) //if -s given
-      str = string
-    else if (file) {
-      str = Utilities.getStringFromFile(this, file)
-    } else
-      str = args.string
-
-    if (!key) {
-      Logger.error(this,'Key is not passed')
-    }
-
-    if (encryption) {
-      if (decryption) // if both given
-        Logger.error(this,'Both encryption and decryption methods passed')
-      this.Encrypt(str, encryption, key)
-    } else if (decryption) {
-      this.Decrypt(str, decryption, key)
-    } else {
-      Logger.error(this,'Neither encryption or decryption methods passed')
-    }
   }
 
-  private Encrypt(str: string, type: string, key: string | undefined) {
-    let crypto = this.getCryptoType(type)
+  private Encrypt(flags: any, args:any) {
 
-    if (crypto) {
-      // @ts-ignore
-      // let encrypted: string = crypto.encrypt(str, key, {
-      //   mode: CryptoJS.mode.CBC}).ciphertext.toString(CryptoJS.enc.Hex)
-      let encrypted: string = crypto.encrypt(str, key).ciphertext
-      this.log(`[${type.toUpperCase()}]: ${encrypted}`)
-    } else {
-      Logger.error(this,'invalid hash type')
-    }
+    let crypto = this.getCryptoType(args.type)
+
+    Logger.info(this,`Encryption: ${flags.encryption.toUpperCase()}`)
+
+    // @ts-ignore // as crypto will never be undefined and reach here
+    let encrypted: string = crypto.encrypt(args.string, flags.key, {
+      mode: this.getCryptoMode(this,flags)
+    }).toString()
+
+     //always add input to args
+
+    Logger.success(this,`${encrypted}`)
   }
 
-  private Decrypt(str: string, type: string, key: string | undefined) {
-    let crypto = this.getCryptoType(type)
+  private Decrypt(flags: any, args: any) {
+    let crypto = this.getCryptoType(args.type)
 
-    if (crypto) {
-      // @ts-ignore
-      let decrypted: string = crypto.decrypt(str, key)
-      this.log(`[${type.toUpperCase()}]: ${decrypted}`)
-    } else {
-      Logger.error(this,'invalid hash type')
-    }
+    Logger.info(this,`Decryption: ${flags.decryption.toUpperCase()}`)
+
+    // @ts-ignore // as crypto will never be undefined and reach here
+    let decrypted: string = crypto.decrypt(args.string, flags.key, {
+      mode: this.getCryptoMode(this,flags)
+    }).toString(CryptoJS.enc.Utf8)
+
+    Logger.success(this,`${decrypted}`)
   }
 
   private getCryptoType(type: string) {
     switch (type.toUpperCase()) {
-    case 'AES':
-      return CryptoJS.AES
-    case 'DES':
-      return CryptoJS.DES
-    default:
-      // tslint:disable-next-line:no-return-undefined
-      return undefined //returning because of check there
+      case 'AES':
+        return CryptoJS.AES
+      case 'DES':
+        return CryptoJS.DES
+      case '3DES':
+        return CryptoJS.TripleDES
+      case 'RABBIT':
+        return CryptoJS.Rabbit
+      case 'RC4':
+        return CryptoJS.RC4
+      case 'RC4DROP':
+        return CryptoJS.RC4Drop
+      default:
+        Logger.error(this,'Invalid or Unsupported Encryption/Decryption type')
+        return undefined // will never reach here
     }
 
   }
+
+  // to check required parameters passed or not
+  private checkParameters(flags: any, args: any) {
+    if (!flags.key)
+      Logger.error(this,'Key is not passed')
+
+    if(args.string == undefined || args.string =="" )
+      Logger.error(this, 'Input string is empty or undefined')
+
+    if (flags.encryption && flags.decryption)
+      Logger.error(this,'Both encryption and decryption methods passed')
+
+    if(!(flags.encryption || flags.decryption))
+      Logger.error(this,'Neither encryption or decryption methods passed')
+  }
+
+  private getCryptoMode(thisRef: any, flags: any) {
+    if(!flags.mode) //set default
+      flags.mode='CBC' // it will not set to flags.mode there in run() but we do not require it
+    Logger.info(this,'Block Mode: '+flags.mode)
+    switch (flags.mode.toUpperCase()) {
+      case 'CBC':
+        return CryptoJS.mode.CBC
+      case 'CFB':
+        return CryptoJS.mode.CFB
+      case 'OFB':
+        return CryptoJS.mode.OFB
+      case 'ECB':
+        return CryptoJS.mode.ECB
+      default:
+        Logger.error(this,'Invalid or Unsupported Block Mode')
+        return undefined // will never reach here
+    }
+  }
+
 }
