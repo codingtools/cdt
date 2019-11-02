01ae044d5a286b041b408c07aa843f0180a4dac5
ashish <ashishpatel0720@gmail.com>
2019-11-02 20:21:49 +0530

    [BUNDLEPHOBIA]:  white space fixed
    Signed-off-by: ashish <ashishpatel0720@gmail.com>

--- test/commands/crypto.test.ts
+++ test/commands/crypto.test.ts
@@ -19,49 +19,49 @@ describe('crypto', () => {
 
   test
     .stdout()
-    .command(['crypto', '-d', 'aes', '-s','U2FsdGVkX1/OLQ6Lp+V3O1d5SaxEf9pAf8CV7ErBC9o=', '-k', 'Secret Passphrase'])
+    .command(['crypto', '-d', 'aes', '-s', 'U2FsdGVkX1/OLQ6Lp+V3O1d5SaxEf9pAf8CV7ErBC9o=', '-k', 'Secret Passphrase'])
     .it('AES Decryption string passed as flag', ctx => {
       expect(ctx.stdout).to.contain('Message')
     })
 
   test
     .stdout()
-    .command(['crypto', '-d', 'des', '-s','U2FsdGVkX180d+J1kUcxGL9bbBAErXAw', '-k', 'Secret Passphrase'])
+    .command(['crypto', '-d', 'des', '-s', 'U2FsdGVkX180d+J1kUcxGL9bbBAErXAw', '-k', 'Secret Passphrase'])
     .it('DES Decryption', ctx => {
       expect(ctx.stdout).to.contain('Message')
     })
 
   test
     .stdout()
-    .command(['crypto', '-d', 'des', '-s','U2FsdGVkX186YglxZ7yF7aqTjFQA3Yzs', '-k', 'Secret Passphrase','-m','ECB'])
+    .command(['crypto', '-d', 'des', '-s', 'U2FsdGVkX186YglxZ7yF7aqTjFQA3Yzs', '-k', 'Secret Passphrase', '-m', 'ECB'])
     .it('DES Decryption with Mode ECB', ctx => {
       expect(ctx.stdout).to.contain('Message')
     })
 
   test
     .stdout()
-    .command(['crypto', '-d', '3des', '-s','U2FsdGVkX1+2jkjCxuWwL8uMgdu6SXJc', '-k', 'Secret Passphrase'])
+    .command(['crypto', '-d', '3des', '-s', 'U2FsdGVkX1+2jkjCxuWwL8uMgdu6SXJc', '-k', 'Secret Passphrase'])
     .it('3DES Decryption', ctx => {
       expect(ctx.stdout).to.contain('Message')
     })
 
   test
     .stdout()
-    .command(['crypto', '-d', 'RABBIT', '-s','U2FsdGVkX185oOsUqvpF+7x0zPUxNJw=', '-k', 'Secret Passphrase'])
+    .command(['crypto', '-d', 'RABBIT', '-s', 'U2FsdGVkX185oOsUqvpF+7x0zPUxNJw=', '-k', 'Secret Passphrase'])
     .it('RABBIT Decryption', ctx => {
       expect(ctx.stdout).to.contain('Message')
     })
 
   test
     .stdout()
-    .command(['crypto', '-d', 'RC4', '-s','U2FsdGVkX1+/oErpaqQQk1Fj2eXwL1o=', '-k', 'Secret Passphrase'])
+    .command(['crypto', '-d', 'RC4', '-s', 'U2FsdGVkX1+/oErpaqQQk1Fj2eXwL1o=', '-k', 'Secret Passphrase'])
     .it('RC4 Decryption', ctx => {
       expect(ctx.stdout).to.contain('Message')
     })
 
   test
     .stdout()
-    .command(['crypto', '-d', 'RC4DROP', '-s','U2FsdGVkX18+D1WNQ64XzaCwkUM6moE=', '-k', 'Secret Passphrase'])
+    .command(['crypto', '-d', 'RC4DROP', '-s', 'U2FsdGVkX18+D1WNQ64XzaCwkUM6moE=', '-k', 'Secret Passphrase'])
     .it('RC4Drop Decryption', ctx => {
       expect(ctx.stdout).to.contain('Message')
     })
@@ -92,7 +92,7 @@ describe('crypto', () => {
 
   test
     .stdout()
-    .command(['crypto', '-e', 'aes','-d', 'aes', '-s', 'Message', '-k', 'Secret Passphrase'])
+    .command(['crypto', '-e', 'aes', '-d', 'aes', '-s', 'Message', '-k', 'Secret Passphrase'])
     .exit(0)
     .it('Both encryption and decryption methods passed', ctx => {
       expect(ctx.stdout).to.contain('Both encryption and decryption methods passed')
@@ -105,6 +105,5 @@ describe('crypto', () => {
     .it('Neither encryption nor decryption method passed', ctx => {
       expect(ctx.stdout).to.contain('Neither encryption or decryption methods passed')
     })
-
 
 })
