015d5d6adadcc3d7a117aa44cf09ac5eca91e7aa
ashish <ashishpatel0720@gmail.com>
2021-02-07 14:03:56 +0530

    helper script updated

--- test/resources/scripts/pr_helper.sh
+++ test/resources/scripts/pr_helper.sh
@@ -3,15 +3,17 @@ PR=159
 # checkout pr
 gh pr checkout $PR
 
+git pull origin release/release-v0.x
 
 # npm install
 npm install
-
+git push
 
 # GIT PUSH and then checks ( validate )
 # watching checks running
 watch -n 3 "gh pr checks $PR"
 
-#MERGE PR
+
+#MERGE PR, delete branch and merge in place of other options
 gh pr merge $PR -d -m
 
